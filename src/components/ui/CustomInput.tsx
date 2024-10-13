e1b05ba88d5536933163d912e2dec6b02e2fff68
deji <dejiwilliams9@gmail.com>
2024-10-13 05:55:05 -0700

    type text and password.

--- src/components/ui/CustomInput.tsx
+++ src/components/ui/CustomInput.tsx
@@ -1,30 +1,52 @@
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+
 interface Form {
   label: string;
   placeholder: string;
   value: string;
-  setValue: React.Dispatch<React.SetStateAction<string>>;
   width: string;
   type: string;
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  showPassword?: boolean;
+  setShowPassword?: React.Dispatch<React.SetStateAction<boolean>>
 }
 
 const CustomInput = ({
   label,
   placeholder,
   value,
-  setValue,
   width,
   type,
+  handleChange,
+  showPassword,
+  setShowPassword
 }: Form) => {
+  const togglePasswordVisibility = () => {
+    console.log('toggle clicked')
+    if(setShowPassword){
+      setShowPassword(!showPassword)
+      console.log(showPassword)
+    }
+  }
   return (
     <div className="text-DarkGrey flex flex-col">
       <label className="text-[12px] font-[400] leading-[18px]">{label}</label>
-      <input
-        className={`${width} py-3 px-4 border-2 border-veryLightGray font-[400] rounded-[8px] text-[16px] outline-none focus:border-Purple focus:shadow-2xl `}
-        type={type}
-        placeholder={placeholder}
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-      />
+      <div className={`relative ${width} rounded-[8px]`}>
+        <input
+          className="py-3 w-full px-4 border-2 border-veryLightGray font-[400] rounded-[8px] text-[16px] outline-none focus:border-Purple focus:shadow-2xl"
+          type={type}
+          placeholder={placeholder}
+          value={value}
+          onChange={handleChange}
+        />
+        {showPassword !== undefined && (
+          showPassword ? (
+            <FaEye className="absolute right-3 top-0 bottom-0 my-auto cursor-pointer" onClick={togglePasswordVisibility} />
+          ) : (
+            <FaEyeSlash className="absolute right-3 top-0 bottom-0 my-auto cursor-pointer" onClick={togglePasswordVisibility} />
+          )
+        )}
+      </div>
     </div>
   );
 };
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,17 +1,40 @@
-import { useState } from 'react'
-import CustomInput from '../components/ui/CustomInput'
-import CustomButton from '../components/ui/CustomButton'
+import { useState } from "react";
+import CustomInput from "../components/ui/CustomInput";
+import CustomButton from "../components/ui/CustomButton";
 
 const LoginPage = () => {
-    const [ email, setEmail ] = useState<string>('')
-    const [ password, setPassword ] = useState<string>('')
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const handleEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+  const handlePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
   return (
     <div>
-      <CustomInput width='w-[13%]' type="text" label='Email' placeholder='Write Your Email Here' value={email} setValue={setEmail} />
-      <CustomInput width='w-[13%]' type="text" label='PassWord' placeholder='Write Your PassWord Here' value={password} setValue={setPassword} />
-      <CustomButton text={'Login'} width={'w-[13%]'} />
+      <CustomInput
+        width="w-[13%]"
+        type="text"
+        label="Email"
+        placeholder="Write Your Email Here"
+        value={email}
+        handleChange={handleEmail}
+      />
+      <CustomInput
+        width="w-[13%]"
+        type={showPassword ? "text" : "password"}
+        showPassword={showPassword}
+        setShowPassword={setShowPassword}
+        label="PassWord"
+        placeholder="Write Your PassWord Here"
+        value={password}
+        handleChange={handlePassword}
+      />
+      <CustomButton text={"Login"} width={"w-[13%]"} />
     </div>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
